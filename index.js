867c8ea53998a1ff668421064e6d2a8489c1c5d6
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-09-25 19:40:37 -0600

    commit tree 364bf7e6ae4a52ed624036c6393a6c36a7a5e580

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function superbowlWin(record) {
+    const results = record.find(record => record.result === "W") 
+    return results ? results.year : undefined 
+    // if (results === true) {
+//         return results.year
+//     } else {
+//         return undefined
+//     }
+}
 
+
+
+// The function should receive 1 argument, an Array of JavaScript Objects
+// Each object has two properties: year and result
+// It should use find() to test each Object to see if the result is "W" — a win!
+// It should return the year when the win occurred (if it occurred at all!)
+// If no win is found, it should return, sadly, undefine
